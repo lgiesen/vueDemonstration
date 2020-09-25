c58dcdc1e1970e7f7e5a1b3376c94ccc8ffecda4
Leo Giesen <leori@Leo.fritz.box>
2020-09-25 14:53:37 +0200

    :heavy_plus_sign: Internationalization (i18n)

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'de',
+      localeDir: 'locales',
+      enableInSFC: false
+    }
+  }
 }
