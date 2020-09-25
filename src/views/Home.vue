16c5414feba4b1ec5052bd4cd9af1e2c186ed5c7
Leo Giesen <leori@Leo.fritz.box>
2020-09-25 15:50:51 +0200

    :tada: HelloWorld.vue renamed and used as Footer.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <!-- Use the Footer component as a tag -->
+    <Footer msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Footer from "@/components/Footer.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    Footer,
+  },
+};
 </script>
