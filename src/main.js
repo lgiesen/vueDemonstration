3cec1240bece63e4f9ab7bdadc920fa5df91dcf9
Leo Giesen <leori@Leo.fritz.box>
2020-09-25 15:29:15 +0200

    :heavy_plus_sign: Vuex and Router

--- src/main.js
+++ src/main.js
@@ -2,11 +2,15 @@ import Vue from "vue";
 import App from "./App.vue";
 import vuetify from './plugins/vuetify';
 import i18n from './i18n'
+import router from './router'
+import store from './store'
 
 Vue.config.productionTip = false;
 
 new Vue({
   vuetify,
   i18n,
+  router,
+  store,
   render: (h) => h(App)
 }).$mount("#app");
