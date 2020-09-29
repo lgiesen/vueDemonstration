57901871418655a558181e274a9e3fe8a1f5af9d
Leo Giesen <leori@Leo.fritz.box>
2020-09-29 20:20:30 +0200

    :sparkles: preparation work for Vuex

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 import Vue from "vue";
+import Vuex from 'vuex';
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
 import i18n from "./i18n";
 import router from "./router.js";
-import store from "./store";
+import store from "./store/store.js";
 
 Vue.config.productionTip = false;
-
+Vue.use(Vuex);
 new Vue({
   vuetify,
   i18n,
