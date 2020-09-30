d62be3d8b16925ebfe7c1999c69d419483a866b7
Leo Giesen <leori@Leo.fritz.box>
2020-09-30 15:49:55 +0200

    :pencil: using v-pre to disable variable interpretation

--- src/components/TheNavigation/TheNavigation.vue
+++ src/components/TheNavigation/TheNavigation.vue
@@ -34,4 +34,16 @@ ul {
     text-decoration: none;
   }
 }
+#nav {
+  padding: 30px;
+  text-align: center;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
 </style>
--- src/views/State_Management.vue
+++ src/views/State_Management.vue
@@ -1,22 +1,25 @@
 <template lang="pug">
   v-container
-    v-row(justify="start")
+    v-row
       v-col(align="center")
-        h1.display-3.warning--text.font-weight-bold
+        h1.display-3.warning--text.font-weight-bold 
           | Vuex - State Management
     v-row
-      p In store.js: state: {no: 11}. 
-      | This is referenced in State_Management.vue (this view)
-      | within 
-      br
-      | computed: {
-      |  num () {
-      |   return this.$store.state.no
-      |  },
-      | }
-      br
-      | This num can be referenced in the html template of the view
-      | with double curly brackets: {{num}}
+      //- v-pre disables vues interpretation of code (here: curly brackets) for that block
+      p(v-pre) In store.js: state: {no: 11}. 
+        | This is referenced in State_Management.vue (this view)
+        | within
+        br
+        | computed: {
+        br
+        |  num () {
+        |   return this.$store.state.no
+        |  },
+        br
+        | }
+        br
+        | This num can be referenced in the html template of the view
+        | with double curly brackets: {{ num }}
 </template>
 
 <script>
