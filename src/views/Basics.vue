81452389994206414b2f8e0ab1a35ce0f0931df8
Leo Giesen <leori@Leo.fritz.box>
2020-10-01 16:21:11 +0200

    :sparkles: adding class and style binding content

--- src/views/Basics.vue
+++ src/views/Basics.vue
@@ -26,12 +26,28 @@
                 v-card-title
                     h2 Class and Style Binding
                 v-card-text
-                    v-btn(:class="{active: isActive, disabled: !isActive}" 
+                    v-btn(
+                        :class="[isActive ? 'active' : 'disabled']" 
                         @click="toggleClass") Toggle my class
-                    p You can pass arguments to the v-bind directive so that 
-                        | you can add classes based on boolean expressions.
-                        | The code used for the button is: 
-                        v-pre v-btn(:class="{active: isActive, disabled: !isActive}" @click="toggleClass") Toggle my class
+                    p
+                        v-pre You can pass arguments to the v-bind directive so that 
+                            | you can add classes based on boolean expressions.
+                            | The code used for the button is: 
+                            | v-btn(:class="{active: isActive, disabled: 
+                            | !isActive}" @click="toggleClass") Toggle my class
+                            | active and disabled are classes, which are applied based on the data isActive
+                            | By pressing the button, you toggle the value of isActive
+                            | and therefore add disabled and remove active or the other way around. 
+                            br 
+                            br
+                            | Luckily, there are more beautiful ways of implementation:
+                            br 
+                            | First one is:
+                            | :class="[isActive ? 'active' : 'disabled']
+                            br
+                            | Second one is:
+                            | :class="checkIfActive" in combination with the computed property:
+                            | checkIfActive(){ return this.isActive ? "active" : "disabled"; }
     v-row
         v-col
             v-card
@@ -83,7 +99,11 @@ export default {
             this.isActive = !this.isActive
         }
     },
-    computed: {}
+    computed: {
+        checkIfActive(){
+            return this.isActive ? "active" : "disabled";
+        }
+    }
 }
 </script>
 
