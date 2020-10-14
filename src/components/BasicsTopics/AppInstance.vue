e12f743c09382696ebfb59baff3c1eb7476f5e0a
lgiesen <leo.richard.giesen@gmail.com>
2020-10-14 19:38:35 +0200

    :sparkles: Adding content for AppInstance and TemplateSyntax

--- src/components/BasicsTopics/AppInstance.vue
+++ src/components/BasicsTopics/AppInstance.vue
@@ -3,7 +3,13 @@
         v-card-title
             h2 Application Instance
         v-card-text
-            p 
+            p The Vue or Application Instance in @/main.js is at the heart of every 
+            | Vue Application because it is created with central options and 
+            | dependencies. During its mounting process, all properties from data 
+            | are loaded in a reactivity system.
+            p Check out the 
+                a(href="https://v3.vuejs.org/guide/instance.html#creating-an-instance") Vue Guide
+                span  for further detail.
 </template>
 
 <script>
--- src/components/BasicsTopics/TemplateSyntax.vue
+++ src/components/BasicsTopics/TemplateSyntax.vue
@@ -3,12 +3,20 @@
         v-card-title
             h2 Template Syntax
         v-card-text
-            p 
+            p The HTML template syntax affords declarative rendering of 
+                | DOM-data, which conveys that calling a variable in DOM is 
+                | reactive. This automatic update of a variable is achieved 
+                | with moustache syntax, which also allows JS logic: 
+            vue-code-highlight {{{ variableName >= 0 ? ‘positive’ : ‘negative’}}}
 </template>
 
 <script>
+import { component as VueCodeHighlight } from 'vue-code-highlight';
 export default {
-  name: "TemplateSyntax"
+  name: "TemplateSyntax",
+  components:{
+    VueCodeHighlight,
+  }
 };
 </script>
 <style lang="scss" scoped>
