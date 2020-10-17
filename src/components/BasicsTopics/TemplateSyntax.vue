37b799808af507e13f3dee8960ed9f36fd8d1971
lgiesen <leo.richard.giesen@gmail.com>
2020-10-17 16:29:21 +0200

    :fire: Deleting failed attempts at code highlighting markdown

--- src/components/BasicsTopics/TemplateSyntax.vue
+++ src/components/BasicsTopics/TemplateSyntax.vue
@@ -7,24 +7,14 @@
               | DOM-data, which conveys that calling a variable in DOM is 
               | reactive. This automatic update of a variable is achieved 
               | with moustache syntax, which also allows JS logic: 
-            MarkdownCode(:markdown="exampleCode")
             highlightjs(language="javascript" :code="exampleCode")
-            pre
-              code.javascript {{exampleCode}}
 </template>
 
 <script>
-import MarkdownCode from "@/components/MarkdownCode.vue";
-
 export default {
   name: "TemplateSyntax",
-  components:{
-    MarkdownCode
-  },
   data: () => ({
     exampleCode: "let x = 3; function() { return test; }"
   }),
-  computed: {
-  }
 };
 </script>
