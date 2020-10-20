1758d2257f9bf2cd294d82a2bb40b2c2a4c4a64e
lgiesen <leo.richard.giesen@gmail.com>
2020-10-20 14:24:03 +0200

    :pencil2: adding content for conditional rendering and computed properties

--- src/components/BasicsTopics/ConditionalRendering.vue
+++ src/components/BasicsTopics/ConditionalRendering.vue
@@ -3,12 +3,23 @@
         v-card-title
             h2 Conditional Rendering
         v-card-text
-            p 
+            p {{ text }}
+            highlightjs(language="html" :code="vif")
+
 </template>
 
 <script>
 export default {
-  name: "ConditionalRendering"
+  name: "ConditionalRendering",
+  data: () => ({
+    vif: `<div(v-if=”bool1”)></div> \n<div(v-else-if=”bool2”)></div> \n<div(v-else)></div>`,
+    text: `The v-if directive determines if a DOM-element is 
+    rendered based on a boolean value. In combination with it, 
+    the v-else-if or the v-else directive can be used. 
+    Although v-if and v-show have the same use cases, the 
+    deciding difference is that v-show only affects the CSS 
+    property display and is always rendered.`
+  })
 };
 </script>
 <style lang="scss" scoped>
